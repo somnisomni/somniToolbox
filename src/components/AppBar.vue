4d9f13c542fd88f49ad67f486dd549a5c01e132e
somni <me@somni.one>
2023-04-17 15:42:43 +0900

    Home/Help button in App bar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,16 +1,57 @@
 <template>
   <VAppBar>
     <VAppBarNavIcon icon="mdi-menu" />
+    <VBtn v-if="!isHomeRoute"
+          :to="{ name: 'home' }"
+          icon="mdi-home"
+          flat />
     <VAppBarTitle>{{ APP_NAME }}</VAppBarTitle>
+
+    <VMenu>
+      <template v-slot:activator="{ props }">
+        <VAppBarNavIcon icon="mdi-dots-vertical" v-bind="props" />
+      </template>
+
+      <VList>
+        <VListItem v-for="item in APP_MENU_DATA"
+                   :key="item.id"
+                   :value="item.id"
+                   :prepend-icon="item.icon"
+                   :to="item.route">
+          <VListItemTitle>{{ item.title }}</VListItemTitle>
+        </VListItem>
+      </VList>
+    </VMenu>
   </VAppBar>
+
 </template>
 
 <script lang="ts">
 import { APP_NAME } from "@/constants";
 import { Component, Vue } from "vue-facing-decorator";
 
+interface AppMenuData {
+  id: string,
+  title: string,
+  icon: string,
+  route: string,
+}
+
 @Component
 export default class AppBar extends Vue {
   readonly APP_NAME = APP_NAME;
+
+  readonly APP_MENU_DATA: AppMenuData[] = [
+    {
+      "id": "help",
+      "title": "Help",
+      "icon": "mdi-help-circle",
+      "route": "/help",
+    },
+  ];
+
+  get isHomeRoute(): boolean {
+    return this.$route.name === "home";
+  }
 }
 </script>
